18a0d7d796c980e521db716da174492cf07c4112
tomiyamasatoshi <n2m71212@yahoo.co.jp>
2021-03-22 23:37:31 +0900

    modify service annotation

--- src/main/java/jp/co/api/myweb/service/ContactService.java
+++ src/main/java/jp/co/api/myweb/service/ContactService.java
@@ -1,10 +1,7 @@
 package jp.co.api.myweb.service;
 
-import org.springframework.stereotype.Service;
-
 import jp.co.api.myweb.request.ContactRequestForm;
 
-@Service
 public interface ContactService {
 
 	public void insertContact(ContactRequestForm contactForm);
--- src/main/java/jp/co/api/myweb/service/QualificationsService.java
+++ src/main/java/jp/co/api/myweb/service/QualificationsService.java
@@ -1,10 +1,7 @@
 package jp.co.api.myweb.service;
 
-import org.springframework.stereotype.Service;
-
 import jp.co.api.myweb.response.QualificationsResponseForm;
 
-@Service
 public interface QualificationsService {
 
 	public QualificationsResponseForm selectQualifications();
